867bcb124a8e2ffcbb3425748153fa04b2162c96
LAIS DA SILVA ALMEIDA <lais.salmeida6@senacsp.edu.br>
2025-02-27 21:04:16 -0300

    feat: api login

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -3,7 +3,6 @@ import FormLogin from "./_components/form";
 export default function Login() {
   return (
     <div className="flex h-screen">
-      {/* Esquerda - Testemunho */}
       <div className="hidden md:flex flex-col justify-center items-center w-1/2 bg-black text-white p-10">
         <div className="max-w-md text-center">
           <p className="text-lg font-semibold">
@@ -13,7 +12,6 @@ export default function Login() {
         </div>
       </div>
       
-      {/* Direita - Formulário de Login */}
       <div className="flex justify-center items-center w-full md:w-1/2 p-8">
         <FormLogin/>
       </div>
